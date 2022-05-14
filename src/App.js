3183a95456e2cae9a0e60b1e25380c83dc6e7567
amir-mohammad <amirm.azarbashi@gmail.com>
2022-05-14 12:11:06 +0430

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes, Outlet } from "react-router-dom";
 import HomePage from "./routes/home/home.component";
 import NavigationBar from './routes/navigation/navigation.component';
-
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,7 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<NavigationBar />}>
         <Route index element={<HomePage />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path='signIn' element={<SignIn />} />
       </Route>
     </Routes>
   );
