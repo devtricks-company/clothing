7d74e5dc21ea574232fea43971ccb488393cfa9e
devtricks-company <amir.mo.azarbashi@gmail.com>
2022-06-03 19:17:20 +0430

    adding cartitem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, Outlet } from "react-router-dom";
 import Authentication from "./routes/authentication/authentication.component";
+import Checkout from "./routes/checkout/checkout.component";
 import HomePage from "./routes/home/home.component";
 import NavigationBar from './routes/navigation/navigation.component';
 import Shop from "./routes/shop/shop.component";
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="shop" element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout/>} />
       </Route>
     </Routes>
   );
